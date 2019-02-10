586ed869d663bccd14226b34347008589a6d69c2
Subrahmanya Prasad <subprasadn@gmail.com>
2019-02-10 18:31:14 +0530

    added1

--- java/net/masterthought/cucumber/ConfigurationTest.java
+++ java/net/masterthought/cucumber/ConfigurationTest.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-
+// aaaa
 /**
  * @author Damian Szczepanik (damianszczepanik@github)
  */
